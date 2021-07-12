ff44365007f46fa16b2e1737b7be870fb0598c83
Mateusz Klys <randle9000@gmail.com>
2021-07-12 10:15:36 +0200

    added post method to create new clening in db

--- backend/app/api/routes/cleanings.py
+++ backend/app/api/routes/cleanings.py
@@ -1,5 +1,11 @@
 from typing import List
-from fastapi import APIRouter
+
+from fastapi import APIRouter, Body, Depends
+from starlette.status import HTTP_201_CREATED
+
+from app.models.cleaning import CleaningCreate, CleaningPublic
+from app.db.repositories.cleanings import CleaningsRepository
+from app.api.dependencies.database import get_repository
 
 router = APIRouter()
 
@@ -19,3 +25,30 @@ async def get_all_cleanings() -> List[dict]: # -> List[dicts] shows what type is
 
     ]
     return cleanings
+
+
+"""
+By simply specifying the CleaningCreate Python type declaration for new_cleaning, FastAPI will:
+
+    Read the body of the request as JSON.
+    Convert the corresponding types.
+    Validate the data.
+    Respond with an error if validation fails, or provide the route with the model instance needed.
+
+The second of the two path parameters - cleanings_repo - 
+is our database interface, and the route's only dependency. 
+We're setting its default to Depends(get_repository(CleaningsRepository)) 
+so that we have access to it in our route and can create the new cleaning 
+whenever the function is executed. 
+FastAPI automatically validates and converts the created_cleaning 
+to an instance of our CleaningPublic model, 
+and sends the appropriate JSON as a response.
+
+"""
+@router.post("/", response_model=CleaningPublic, name="cleanings:create-cleaning", status_code=HTTP_201_CREATED)
+async def create_new_cleaning(
+    new_cleaning: CleaningCreate = Body(..., embed=True),
+    cleanings_repo: CleaningsRepository = Depends(get_repository(CleaningsRepository)),
+) -> CleaningPublic:
+    created_cleaning = await cleanings_repo.create_cleaning(new_cleaning=new_cleaning)
+    return created_cleaning
