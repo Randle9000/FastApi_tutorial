b1930f58a1022f7cfc48074a2e13c9e902691067
Mateusz Klys <randle9000@gmail.com>
2021-07-13 11:27:32 +0200

    added tests to test the wrong id returns error

--- backend/tests/test_cleanings.py
+++ backend/tests/test_cleanings.py
@@ -1,3 +1,5 @@
+import logging
+
 import pytest
 
 from httpx import AsyncClient
@@ -9,6 +11,8 @@ from app.models.cleaning import CleaningCreate, CleaningInDB
 
 # decorate all tests with @pytest.mark.asyncio
 pytestmark = pytest.mark.asyncio
+
+logger = logging.getLogger(__name__).info
 
 @pytest.fixture
 def new_cleaning():
@@ -64,8 +68,22 @@ class TestCreateCleaning:
 
 
 class TestGetCleaning:
-    async def test_get_cleaning_by_id(self, app: FastAPI, client: AsyncClient) -> None:
-        res = await client.get(app.url_path_for("cleanings:get-cleaning-by-id", id=1))
+    async def test_get_cleaning_by_id(self, app: FastAPI, client: AsyncClient, test_cleaning: CleaningInDB) -> None:
+        res = await client.get(app.url_path_for("cleanings:get-cleaning-by-id", id=test_cleaning.id))
         assert res.status_code == HTTP_200_OK
         cleaning = CleaningInDB(**res.json())
-        assert cleaning.id == 1
+        assert cleaning == test_cleaning
+
+    @pytest.mark.parametrize(
+        "id, status_code",
+        (
+            (500, 404),
+            (-1, 404),
+            (None, 422),
+        ),
+    )
+    async def test_wrong_id_returns_error(
+        self, app: FastAPI, client: AsyncClient, id: int, status_code: int
+    ) -> None:
+        res = await client.get(app.url_path_for("cleanings:get-cleaning-by-id", id=id))
+        assert res.status_code == status_code
