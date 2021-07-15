b3f2c9db238c60393f0b72d8bb5e89fe53b97f7f
Mateusz Klys <randle9000@gmail.com>
2021-07-15 15:45:16 +0200

    created models for users

--- backend/app/models/core.py
+++ backend/app/models/core.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+from pydantic import BaseModel, validator
 
 
 class CoreModel(BaseModel):
@@ -6,6 +8,23 @@ class CoreModel(BaseModel):
     Any common logic to be shared by all models goes here
     """
     pass
+
+
+class DateTimeModelMixin(BaseModel):
+    """
+    new common model that will help us manage timestamps across our application
+    """
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
+
+
+    """
+    feature - the validator decorator - to set a default datetime for both the created_at and updated_at fields. 
+    https://pydantic-docs.helpmanual.io/usage/validators/
+    """
+    @validator("created_at", "updated_at", pre=True)
+    def default_datetime(cls, value: datetime) -> datetime:
+        return value or datetime.datetime.now()
 
 
 class IDModelMixin(BaseModel):
---
+++ backend/app/models/user.py
@@ -0,0 +1,70 @@
+import string
+from typing import Optional
+
+from pydantic import EmailStr, constr, validator
+
+from app.models.core import DateTimeModelMixin, IDModelMixin, CoreModel
+
+
+def validate_username(username: str) -> str:
+    allowed = string.ascii_letters + string.digits + '-' + '_'
+    assert all(char in allowed for char in username), "invalid character in user name."
+    assert len(username) >= 3, "Username must be 3 characters or more."
+    return username
+
+
+class UserBase(CoreModel):
+    """
+    Leaving off password and salt from base model
+    """
+    email: Optional[EmailStr]
+    username: Optional[str]
+    email_verified: bool = False
+    is_active: bool = True
+    is_superuser: bool = False
+
+
+class UserCreate(CoreModel):
+    """
+    Email, username, and password are required for registering a new user
+    The constr type is one of pydantic's constrained types and it stands for constrained string. Constrained strings offer the ability to set minimum and maximum lengths on any string value, along with other validations
+    """
+    email: EmailStr
+    password: constr(min_length=7, max_length=100)
+    username: str
+
+    @validator("username", pre=True)
+    def username_is_valid(cls, username: str) -> str:
+        return validate_username(username)
+
+
+class UserUpdate(CoreModel):
+    """
+    Users are allowed to update their email and/or username
+    """
+    email: EmailStr
+    username: str
+
+    @validator("username", pre=True)
+    def username_is_valid(cls, username: str) -> str:
+        return validate_username(username)
+
+
+class UserPasswordUpdate(CoreModel):
+    """
+    Users are allowed to change their password
+    """
+    password: constr(min_length=7, max_length=100)
+    salt: str
+
+
+class UserInDb(IDModelMixin, DateTimeModelMixin, UserBase):
+    """
+    ADd in id, created_at, updated_at, and user's passwrod and salt
+    """
+    password: constr(min_length=7, max_length=100)
+    salt: str
+
+
+class UserPublic(IDModelMixin, DateTimeModelMixin, UserBase):
+    pass
