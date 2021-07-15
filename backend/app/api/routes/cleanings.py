b0f4e0037a3e04b22f6b6fbbe25feafbc5b2325b
Mateusz Klys <randle9000@gmail.com>
2021-07-15 12:21:22 +0200

    added methods to get all cleanings and to update cleaning

--- backend/app/api/routes/cleanings.py
+++ backend/app/api/routes/cleanings.py
@@ -1,9 +1,9 @@
 from typing import List
 
-from fastapi import APIRouter, Body, Depends, HTTPException
+from fastapi import APIRouter, Body, Depends, HTTPException, Path
 from starlette.status import HTTP_201_CREATED, HTTP_200_OK, HTTP_404_NOT_FOUND
 
-from app.models.cleaning import CleaningCreate, CleaningPublic
+from app.models.cleaning import CleaningCreate, CleaningPublic, CleaningUpdate
 from app.db.repositories.cleanings import CleaningsRepository
 from app.api.dependencies.database import get_repository
 
@@ -17,14 +17,14 @@ The @router.get("/") tells FastAPI that the function right below is in charge of
     using a get operation
 
 """
-@router.get("/")
-async def get_all_cleanings() -> List[dict]: # -> List[dicts] shows what type is return as in java :P
-    cleanings = [
-        {"id": 1, "name": "My house", "cleaning_type": "full_clean", "price_per_hour": 29.99},
-        {"id": 2, "name": "Someone else's house", "cleaning_type": "spot clea", "price_per_hour": 19.99}
-
-    ]
-    return cleanings
+# @router.get("/")
+# async def get_all_cleanings() -> List[dict]: # -> List[dicts] shows what type is return as in java :P
+#     cleanings = [
+#         {"id": 1, "name": "My house", "cleaning_type": "full_clean", "price_per_hour": 29.99},
+#         {"id": 2, "name": "Someone else's house", "cleaning_type": "spot clea", "price_per_hour": 19.99}
+#
+#     ]
+#     return cleanings
 
 
 """
@@ -51,6 +51,19 @@ to an instance of our CleaningPublic model,
 and sends the appropriate JSON as a response.
 
 """
+
+
+# @router.get("/", response_model=List[CleaningPublic], name="cleanings:get-all-cleanings")
+# async def get_all_cleanings() -> List[CleaningPublic]:
+#     return [{ "id": 1, "name": "fake cleaning", "price": 0}]
+
+@router.get("/", response_model=List[CleaningPublic], name="cleanings:get-all-cleanings")
+async def get_all_cleanings(
+        cleanings_repo: CleaningsRepository = Depends(get_repository(CleaningsRepository)),
+) -> List[CleaningPublic]:
+    return await cleanings_repo.get_all_cleanings()
+
+
 @router.post("/", response_model=CleaningPublic, name="cleanings:create-cleaning", status_code=HTTP_201_CREATED)
 async def create_new_cleaning(
     new_cleaning: CleaningCreate = Body(..., embed=True),
@@ -70,3 +83,19 @@ async def get_cleanings_by_id(
         raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="No cleaning found with that id.")
 
     return cleaning
+
+
+@router.put("/{id}/", response_model=CleaningPublic, name="cleanings:update-cleaning-by-id")
+async def update_cleanings_by_id(
+        id: int = Path(..., ge=1, title="The id of the cleaning to update"), # ge means the cleaning id must be integer greater or equal to 1
+        cleaning_update: CleaningUpdate = Body(..., embed=True),
+        cleanings_repo: CleaningsRepository = Depends(get_repository(CleaningsRepository)),
+) -> CleaningPublic:
+    updated_cleaning = await cleanings_repo.update_cleaning(id=id, cleaning_update=cleaning_update)
+
+    if not updated_cleaning:
+        raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail="No cleaning found with that id")
+
+    return updated_cleaning
+
+
