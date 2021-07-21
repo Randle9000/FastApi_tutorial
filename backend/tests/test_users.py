d0ad05abe8879e888b5ee7d19759fc46c0309dd1
Mateusz Klys <randle9000@gmail.com>
2021-07-21 10:10:30 +0200

    Further work with authentication: - added services to handle authentication operations - added new requirements pyjwt passlib - added new test related to the user auth - replaced implementation of register new user, previous the salt was not generated randomly,now it is.

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -14,6 +14,7 @@ from starlette.status import (
 
 from app.models.user import UserCreate, UserInDB
 from app.db.repositories.users import UsersRepository
+from app.services import auth_service
 
 
 from databases import Database
@@ -84,3 +85,29 @@ class TestUserRegistration:
         res = await client.post(app.url_path_for("users:register-new-user"), json={"new_user": new_user})
 
         assert res.status_code == status_code
+
+    async def test_users_saved_password_is_hashed_and_has_salt(
+        self,
+        app: FastAPI,
+        client: AsyncClient,
+        db: Database,
+    ) -> None:
+        user_repo = UsersRepository(db)
+        new_user = {"email": "hipopotam@zoowroc.pl", "username": "Ryszard_Hip", "password": "the_qwe2r"}
+
+
+        # send post request to create user and ensure it is successful
+        res = await client.post(app.url_path_for("users:register-new-user"), json={"new_user": new_user})
+        assert res.status_code == HTTP_201_CREATED
+
+        # ensure that the users password is hashed in the db
+        # and that we can verify it using our auth service
+        user_in_db = await user_repo.get_user_by_email(email=new_user["email"])
+        assert user_in_db is not None
+        assert user_in_db.salt is not None and user_in_db.salt != '123'
+        assert user_in_db.password != new_user["password"]
+        assert auth_service.verify_password(
+            password=new_user["password"],
+            salt=user_in_db.salt,
+            hashed_pw=user_in_db.password,
+        )
