8e5f2cd4f409ba0d2c33bbf56d31e38ed8634802
Mateusz Klys <randle9000@gmail.com>
2021-07-26 13:29:31 +0200

    added following tests: - test_authenticated_user_can_retrieve_own_data - test_user_cannot_access_own_data_if_not_authenticated - test_user_cannot_access_own_data_with_incorrect_jwt_prefix - test_error_when_token_or_secret_is_wrong - test_can_retrieve_username_from_token

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -26,6 +26,8 @@ from app.services import auth_service
 from app.core.config import SECRET_KEY, JWT_AUDIENCE, JWT_ALGORITHM, JWT_TOKEN_PREFIX, ACCESS_TOKEN_EXPIRE_MINUTES
 from app.models.token import JWTMeta, JWTCreds, JWTPayload
 
+from fastapi import FastAPI, HTTPException, status
+
 from databases import Database
 
 pytestmark = pytest.mark.asyncio
@@ -187,6 +189,41 @@ class TestAuthTokens:
             )
             jwt.decode(access_token, str(SECRET_KEY), audience=JWT_AUDIENCE, algorithms=[JWT_ALGORITHM])
 
+    async def test_can_retrieve_username_from_token(
+            self,
+            app: FastAPI,
+            client: AsyncClient,
+            test_user: UserInDB,
+    ) -> None:
+        token = auth_service.create_access_token_for_user(user=test_user, secret_key=str(SECRET_KEY))
+        username = auth_service.get_username_from_token(token=token, secret_key=str(SECRET_KEY))
+        assert username == test_user.username
+
+    @pytest.mark.parametrize(
+        "secret, wrong_token",
+        (
+            (SECRET_KEY, "asdf"),  # use wrong token
+            (SECRET_KEY, ""),  # use wrong token
+            (SECRET_KEY, None),  # use wrong token
+            ("ABC123", "use correct token"),  # use wrong secret
+        ),
+    )
+    async def test_error_when_token_or_secret_is_wrong(
+            self,
+            app: FastAPI,
+            client: AsyncClient,
+            test_user: UserInDB,
+            secret: Union[Secret, str],
+            wrong_token: Optional[str],
+    ) -> None:
+        token = auth_service.create_access_token_for_user(user=test_user, secret_key=str(SECRET_KEY))
+
+        if wrong_token == "user correct token":
+            wrong_token = token
+
+        with pytest.raises(HTTPException):
+            username = auth_service.get_username_from_token(token=wrong_token, secret_key=str(secret))
+
 
 class TestUserLogin:
     """
@@ -250,3 +287,41 @@ class TestUserLogin:
         res = await client.post(app.url_path_for("users:login-email-and-password"), data=login_data) # data instead json
         assert res.status_code == status_code
         assert "access_token" not in res.json()
+
+
+class TestUserMe:
+    async def test_authenticated_user_can_retrieve_own_data(
+            self,
+            app: FastAPI,
+            authorized_client: AsyncClient,
+            test_user: UserInDB,
+    ) -> None:
+        res = await authorized_client.get(app.url_path_for("users:get-current-user"))
+        assert res.status_code == HTTP_200_OK
+        user = UserPublic(**res.json())
+        assert user.email == test_user.email
+        assert user.username == test_user.username
+        assert user.id == test_user.id
+
+    async def test_user_cannot_access_own_data_if_not_authenticated(
+            self,
+            app: FastAPI,
+            client: AsyncClient,
+            test_user: UserInDB,
+    ) -> None:
+        res = await client.get(app.url_path_for("users:get-current-user"))
+        assert res.status_code == HTTP_401_UNAUTHORIZED
+
+    @pytest.mark.parametrize("jwt_prefix", (("",), ("value",), ("Token",), ("JWT",), ("Swearer",),))
+    async def test_user_cannot_access_own_data_with_incorrect_jwt_prefix(
+            self,
+            app: FastAPI,
+            client: AsyncClient,
+            test_user: UserInDB,
+            jwt_prefix: str,
+    ) -> None:
+        token = auth_service.create_access_token_for_user(user=test_user, secret_key=str(SECRET_KEY))
+        res = await client.get(
+            app.url_path_for("users:get-current-user"), headers={"Authorization": f"{jwt_prefix} {token}"}
+        )
+        assert res.status_code == HTTP_401_UNAUTHORIZED
