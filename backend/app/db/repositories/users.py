20d9ad18cdc1e7e02563113b8ebf719d53c2eaf7
Mateusz Klys <randle9000@gmail.com>
2021-07-28 09:36:24 +0200

    Added mechanism which for each registration of new user will trigger the creation of profile record in db

--- backend/app/db/repositories/users.py
+++ backend/app/db/repositories/users.py
@@ -6,15 +6,17 @@ from databases import Database
 from app.db.repositories.base import BaseRepository
 from app.models.user import UserCreate, UserUpdate, UserInDB, UserPublic
 from app.services import auth_service
+from app.db.repositories.profiles import ProfilesRepository
+from app.models.profile import ProfileCreate
 
-#TODO
-"""
-!!!!
-REMINDER: NEVER STORE PASSWORDS IN PLAIN TEXT LIKE WE ARE HERE. There's 
-a reason we're hardcoding in a fake salt here. For actual authentication, 
-we'll want to generate a salt and hash the password with passlib and bcrypt.
-!!!!
-"""
+
+
+#  !!!!
+#  REMINDER: NEVER STORE PASSWORDS IN PLAIN TEXT LIKE WE ARE HERE. There's
+#  a reason we're hardcoding in a fake salt here. For actual authentication,
+#  we'll want to generate a salt and hash the password with passlib and bcrypt.
+#  !!!!
+
 
 GET_USER_BY_EMAIL_QUERY="""
 SELECT id, username, email, email_verified, password, salt, is_active, is_superuser, created_at, updated_at
@@ -39,6 +41,7 @@ class UsersRepository(BaseRepository):
     def __init__(self, db: Database) -> None:
         super().__init__(db)
         self.auth_service = auth_service
+        self.profiles_repo = ProfilesRepository(db)
 
     async def get_user_by_email(self, *, email: str) -> UserInDB:
         user_record = await self.db.fetch_one(query=GET_USER_BY_EMAIL_QUERY, values={"email": email})
@@ -72,7 +75,7 @@ class UsersRepository(BaseRepository):
         user_password_update = self.auth_service.create_salt_and_hashed_password(plaintext_password=new_user.password)
         new_user_params = new_user.copy(update=user_password_update.dict())
         created_user = await self.db.fetch_one(query=REGISTER_NEW_USER_QUERY, values=new_user_params.dict())
-
+        await self.profiles_repo.create_profile_for_user(profile_create=ProfileCreate(user_id=created_user["id"]))
         return UserInDB(**created_user)
 
     async def authenticate_user(self, *, email: EmailStr, password: str) -> Optional[UserInDB]:
