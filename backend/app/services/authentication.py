459edfd16a935b7b385b25f3897e311f2f197a8b
Mateusz Klys <randle9000@gmail.com>
2021-07-28 13:09:22 +0200

    changed type for user from UserInDB to UserBase because access token can be created for both classes UserInDB and UserPublic and parent class of them is UserInDB

--- backend/app/services/authentication.py
+++ backend/app/services/authentication.py
@@ -2,11 +2,11 @@ import jwt
 import bcrypt
 from datetime import datetime, timedelta
 from passlib.context import CryptContext
-from typing import Optional
+from typing import Optional, Type
 
 from app.core.config import SECRET_KEY, JWT_ALGORITHM, JWT_AUDIENCE, JWT_TOKEN_PREFIX, ACCESS_TOKEN_EXPIRE_MINUTES
 from app.models.token import JWTMeta, JWTCreds, JWTPayload
-from app.models.user import UserPasswordUpdate, UserInDB
+from app.models.user import UserPasswordUpdate, UserInDB, UserBase, UserPasswordUpdate
 from fastapi import HTTPException, status
 from pydantic import ValidationError
 
@@ -43,12 +43,12 @@ class AuthService:
     def create_access_token_for_user(
             self,
             *,
-            user: UserInDB,
+            user: Type[UserBase],
             secret_key: str = str(SECRET_KEY),
             audience: str = JWT_AUDIENCE,
             expires_in: int = ACCESS_TOKEN_EXPIRE_MINUTES,
     ) -> str:
-        if not user or not isinstance(user, UserInDB):
+        if not user or not isinstance(user, UserBase):
             return None
 
         jwt_meta = JWTMeta(
