a8a7e37d951a9d2d1613b691587cb276ccfbe228
Mateusz Klys <randle9000@gmail.com>
2021-07-28 18:36:04 +0200

    added user to the fixture test_cleaning and pass user to the create_cleaning to fulfill the model and table requirement

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -59,7 +59,7 @@ async def client(app: FastAPI) -> AsyncClient:
 
 
 @pytest.fixture
-async def test_cleaning(db: Database) -> CleaningInDB:
+async def test_cleaning(db: Database, test_user: UserInDB) -> CleaningInDB:
     cleaning_repo = CleaningsRepository(db)
     new_cleaning = CleaningCreate(
         name="fake cleaning name",
@@ -67,7 +67,7 @@ async def test_cleaning(db: Database) -> CleaningInDB:
         price=9.99,
         cleaning_type="spot_clean",
     )
-    return await cleaning_repo.create_cleaning(new_cleaning=new_cleaning)
+    return await cleaning_repo.create_cleaning(new_cleaning=new_cleaning, requesting_user=test_user)
 
 
 @pytest.fixture
