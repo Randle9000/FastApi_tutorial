2ccead5777fb8c76baffddcb53d45b60ebbb31d0
Mateusz Klys <randle9000@gmail.com>
2021-07-29 16:01:34 +0200

    - update of query UPDATE_CLEANING_BY_ID - change method in list_all_user_cleanings from fetch_one to fetch all - update of update_cleaning method

--- backend/app/db/repositories/cleanings.py
+++ backend/app/db/repositories/cleanings.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from fastapi import HTTPException
-from starlette.status import HTTP_400_BAD_REQUEST
+from starlette import status
 
 from app.db.repositories.base import BaseRepository
 from app.models.cleaning import CleaningCreate, CleaningUpdate, CleaningInDB, CleaningPublic
@@ -30,14 +30,14 @@ SELECT id, name, description, price, cleaning_type
 FROM cleanings;
 """
 
-UPDATE_CLEANING_BY_ID="""
-    UPDATE cleanings  
-    SET name         = :name,  
-        description  = :description,  
-        price        = :price,  
-        cleaning_type = :cleaning_type  
-    WHERE id = :id  
-    RETURNING id, name, description, price, cleaning_type;  
+UPDATE_CLEANING_BY_ID = """
+    UPDATE cleanings
+    SET name         = :name,
+        description  = :description,
+        price        = :price,
+        cleaning_type = :cleaning_type
+    WHERE id = :id AND owner = :owner
+    RETURNING id, name, description, price, cleaning_type, owner, created_at, updated_at;
 """
 
 DELETE_CLEANING_BY_ID_QUERY = """
@@ -68,34 +68,44 @@ class CleaningsRepository(BaseRepository):
         return CleaningInDB(**cleaning)
 
     async def list_all_user_cleanings(self, *, requesting_user: UserInDB) -> List[CleaningInDB]:
-        cleaning_records = await self.db.fetch_one(
+        cleaning_records = await self.db.fetch_all(
             query=LIST_ALL_USER_CLEANINGS_QUERY, values={"owner": requesting_user.id}
         )
 
-        return [CleaningInDB(**l) for l in cleaning_records ]
+        return [CleaningInDB(**l) for l in cleaning_records]
 
     async def get_all_cleanings(self) -> List[CleaningInDB]:
         cleanings_records = await self.db.fetch_all(query=GET_ALL_CLEANINGS)
         return [CleaningInDB(**cleaning) for cleaning in cleanings_records]
 
-    async def update_cleaning(self, *, id: int, cleaning_update: CleaningUpdate) -> CleaningInDB:
-        cleaning = await self.get_cleaning_by_id(id=id)
+    async def update_cleaning(
+            self, *, id: int, cleaning_update: CleaningUpdate, requesting_user: UserInDB
+    ) -> CleaningInDB:
+        cleaning = await self.get_cleaning_by_id(id=id, requesting_user=requesting_user)
 
         if not cleaning:
             return None
 
+        if cleaning.owner != requesting_user.id:
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Users are only able to update cleanings that they created.",
+            )
+
         cleaning_update_params = cleaning.copy(update=cleaning_update.dict(exclude_unset=True)) #By specifying exclude_unset=True, Pydantic will leave out any attributes that were not explicitly set when the model was created.
         if cleaning_update_params.cleaning_type is None:
-             raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="Invalid cleaning type. Cannot be None.")
+             raise HTTPException(
+                 status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid cleaning type. Cannot be None."
+             )
 
-        try:
-            updated_cleaning = await self.db.fetch_one(
-                query=UPDATE_CLEANING_BY_ID, values=cleaning_update_params.dict()
-            )
-            return CleaningInDB(**updated_cleaning)
-        except Exception as e:
-            print(e)
-            raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail="Invalid update params.")
+        updated_cleaning = await self.db.fetch_one(
+            query=UPDATE_CLEANING_BY_ID,
+            values={
+                **cleaning_update_params.dict(exclude={"created_at", "updated_at"}),
+                "owner": requesting_user.id,
+            },
+        )
+        return CleaningInDB(**updated_cleaning)
 
     async def delete_cleaning_by_id(self, id: int):
         cleaning = await self.get_cleaning_by_id(id=id)
